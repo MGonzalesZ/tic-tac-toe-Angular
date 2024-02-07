5da7a108919b7a3eb7c49d5f549d9fb5885e0fb3
MGonzalesZ <mauriciogzb1322@gmail.com>
2024-02-07 00:22:18 -0400

    logica inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,95 @@ export class AppComponent {
   jugador = 'O';
 
   pintar(fila: number, columna: number) {
-    this.posiciones[fila][columna] = this.jugador;
+    if (this.libre(fila, columna)) {
+      this.posiciones[fila][columna] = this.jugador;
+
+      this.verificar_ganador(this.jugador);
+      this.cambiar_jugador();
+    }
+  }
+
+  libre(fila: number, columna: number): boolean {
+    if (this.posiciones[fila][columna] == '?') return true;
+    else return false;
+  }
+
+  cambiar_jugador() {
+    if (this.jugador == 'O') this.jugador = 'X';
+    else this.jugador = 'O';
+  }
+
+  verificar_ganador(jugador: string) {
+    if (
+      this.posiciones[0][0] == jugador &&
+      this.posiciones[0][1] == jugador &&
+      this.posiciones[0][2] == jugador
+    ) {
+      alert(`Ganó el jugador ${jugador}`);
+    }
+
+    if (
+      this.posiciones[1][0] == jugador &&
+      this.posiciones[1][1] == jugador &&
+      this.posiciones[1][2] == jugador
+    ) {
+      alert(`Ganó el jugador ${jugador}`);
+    }
+
+    if (
+      this.posiciones[2][0] == jugador &&
+      this.posiciones[2][1] == jugador &&
+      this.posiciones[2][2] == jugador
+    ) {
+      alert(`Ganó el jugador ${jugador}`);
+    }
+
+    if (
+      this.posiciones[0][0] == jugador &&
+      this.posiciones[1][0] == jugador &&
+      this.posiciones[2][0] == jugador
+    ) {
+      alert(`Ganó el jugador ${jugador}`);
+    }
+
+    if (
+      this.posiciones[0][1] == jugador &&
+      this.posiciones[1][1] == jugador &&
+      this.posiciones[2][1] == jugador
+    ) {
+      alert(`Ganó el jugador ${jugador}`);
+    }
+
+    if (
+      this.posiciones[0][2] == jugador &&
+      this.posiciones[1][2] == jugador &&
+      this.posiciones[2][2] == jugador
+    ) {
+      alert(`Ganó el jugador ${jugador}`);
+    }
+
+    if (
+      this.posiciones[0][0] == jugador &&
+      this.posiciones[1][1] == jugador &&
+      this.posiciones[2][2] == jugador
+    ) {
+      alert(`Ganó el jugador ${jugador}`);
+    }
+
+    if (
+      this.posiciones[0][2] == jugador &&
+      this.posiciones[1][1] == jugador &&
+      this.posiciones[2][0] == jugador
+    ) {
+      alert(`Ganó el jugador ${jugador}`);
+    }
+  }
+
+  reiniciar() {
+    for (let fila = 0; fila <= this.posiciones.length; fila++) {
+      for (let columna = 0; columna <= this.posiciones.length; columna++) {
+        this.posiciones[fila][columna] = '?';
+      }
+    }
   }
 }
