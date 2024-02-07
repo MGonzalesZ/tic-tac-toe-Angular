aa52076b3acefc013f534118e418252e5182f4ed
MGonzalesZ <mauriciogzb1322@gmail.com>
2024-02-07 00:04:36 -0400

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'proyecto';
+  posiciones = [
+    ['?', '?', '?'],
+    ['?', '?', '?'],
+    ['?', '?', '?'],
+  ];
+
+  jugador = 'O';
+
+  pintar(fila: number, columna: number) {
+    this.posiciones[fila][columna] = this.jugador;
+  }
 }
